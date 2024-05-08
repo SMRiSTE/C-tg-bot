16d887bfa2b9f6fdc3a9e8eb3ba23f92d98fe243
Ш.М.Р. <s.m.r.00800ist.@gmail.com>
2024-05-08 16:58:54 +0300

    Initial commit

--- tgbot/tgbot/tgbot.cpp
+++ tgbot/tgbot/tgbot.cpp
@@ -1,9 +1,7 @@
-﻿
-#include <tgbot/tgbot.h>
+﻿#include <tgbot/tgbot.h>
 
 
 int main() {
-    setlocale(LC_ALL, "ru_RU.UTF-8");
 
     TgBot::Bot bot("7006433103:AAGmJU1DGdxUrLQ31OdzluHaADTN-oSceBk");
 
@@ -11,8 +9,7 @@ int main() {
 
     bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
         bot.getApi().sendMessage(message->chat->id, "Hello, enter the day of the week, and I will send the current schedule for you (ISIP-923 groups).");
-
-        });
+    });
 
     bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
 
@@ -24,7 +21,7 @@ int main() {
             return;
         }
         else if (StringTools::startsWith(message->text, "tuesday")) {
-            bot.getApi().sendMessage(message->chat->id, "1. 8:30-10:00. Botalova Nadezhda Fedorovna - 210 / Luxurious Olga Vladimirovna - 213\n\n2. 10:10-11:40. Anna Vasilyevna Parfenova - 205\n\n3. 12:20-13:13-50. Azovtseva Vera Vladimirovna - 202a\n\n4. 14:00-15:30. Anna Vasilyevna Parfenova - 205");
+            bot.getApi().sendMessage(message->chat->id, "1. 8:30-10:00. Botalova Nadezhda Fedorovna - 210 / Luxurious Olga Vladimirovna - 213\n\n2. 10:10 - 11 : 40. Anna Vasilyevna Parfenova - 205\n\n3. 12 : 20 - 13 : 13 - 50. Azovtseva Vera Vladimirovna - 202a\n\n4. 14 : 00 - 15 : 30. Anna Vasilyevna Parfenova - 205");
             return;
         }
         else if (StringTools::startsWith(message->text, "wednesday")) {
